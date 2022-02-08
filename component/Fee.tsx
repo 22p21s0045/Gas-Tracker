e5c26ba4a7818310c26414ad4c98d25cabee8d03
22p21s0045-อัครวิทย์ <71715345+22p21s0045@users.noreply.github.com>
2022-02-08 22:53:41 +0700

    'create-box-fee'

--- component/Fee.tsx
+++ component/Fee.tsx
@@ -3,6 +3,14 @@ import { useQuery } from "react-query";
 import axios from "axios";
 import { Image, Shimmer } from "react-shimmer";
 import { ShimmerThumbnail } from "react-shimmer-effects";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBBtn,
+} from "mdb-react-ui-kit";
+import { MDBRow, MDBCol } from "mdb-react-ui-kit";
 function Fee() {
   async function getFee() {
     const { data } = await axios.get(
@@ -10,7 +18,7 @@ function Fee() {
     );
     return data;
   }
-  getFee();
+
   const { data, error, isError, isLoading } = useQuery("fee", getFee);
   if (isError) {
     return <div>Error</div>;
@@ -22,12 +30,38 @@ function Fee() {
       </div>
     );
   }
-
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+  let array: [] = [data];
+  let maps = array.map((item: any) => {
+    return (
+      <div>
+        <MDBCol md="4" sm="4" lg="4" className = "BoxFee">
+          <h1>เร็ว</h1>
+          <MDBCard style={{ maxWidth: '22px' }}>
+            <MDBCardBody >
+              <MDBCardText>{item.fastestFee}</MDBCardText>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol md="4" sm="4" lg="4" className = "BoxFee">
+          <h1>ปกติ</h1>
+          <MDBCard>
+            <MDBCardBody >
+              <MDBCardText>{item.halfHourFee}</MDBCardText>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol md="4" sm="4" lg="4" className = "BoxFee">
+          <h1>ช้า</h1>
+          <MDBCard>
+            <MDBCardBody >
+              <MDBCardText>{item.hourFee}</MDBCardText>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+        </div>
+    );
+  });
+  return <div>{maps}</div>;
 }
 
 export default Fee;
