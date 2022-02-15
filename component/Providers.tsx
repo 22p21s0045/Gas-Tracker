d77383fb05254f206804361ea645b04ff5356ddd
22p21s0045-อัครวิทย์ <71715345+22p21s0045@users.noreply.github.com>
2022-02-15 14:33:51 +0700

    add-head-success

--- component/Providers.tsx
+++ component/Providers.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Box from '@mui/material/Box';
+import Image from 'next/image';
+import Grid from '@mui/material/Grid';
 function Providers() {
   return (
     <div>
         <Box className='BoxProvider' sx ={{ width:916,height:513}}>
-            <h1 className="Providers">DATA Provider</h1>
+            <h1 className="ProvidersHead">DATA Provider</h1>
         </Box>
 
 
