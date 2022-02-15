48e9836ad58c54b48ca015eac6ed88617bee87ed
22p21s0045-อัครวิทย์ <71715345+22p21s0045@users.noreply.github.com>
2022-02-15 22:46:49 +0700

    add-tsparticle

---
+++ component/TsParticle.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+import Particles from "react-tsparticles";
+function TsParticle() {
+  return (
+    <div className="Particlejs">
+      <Particles
+        options={{
+            background: {
+                color: {
+                  value: "#FBE5E5",
+                },
+              },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#573391",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 0.5,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+          images: {
+            enable: true,
+            src: "https://cf.shopee.co.th/file/f1f4c7cdb8f2f1a0a3a1d6d1e817c097"
+            },
+        }}
+      />
+    </div>
+  );
+}
+
+export default TsParticle;
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import Fee from "../component/Fee";
 import FeeEth from "./FeeEth";
 import { QueryClient, QueryClientProvider } from "react-query";
 import Providers from "../component/Providers";
+import TsParticle from "../component/TsParticle";
 const Home: NextPage = () => {
   const query = new QueryClient();
   return (
@@ -16,7 +17,9 @@ const Home: NextPage = () => {
       <QueryClientProvider client={query}>
         <Fee />
         <Providers/>
+        <TsParticle/>
       </QueryClientProvider>
+      
     </div>
   );
 };
