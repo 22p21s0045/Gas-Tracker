8ec4d98dfb1788722c832782944820e8600bd2c5
22p21s0045-อัครวิทย์ <71715345+22p21s0045@users.noreply.github.com>
2022-02-15 15:10:05 +0700

    add-image-success

--- component/Providers.tsx
+++ component/Providers.tsx
@@ -7,6 +7,15 @@ function Providers() {
     <div>
         <Box className='BoxProvider' sx ={{ width:916,height:513}}>
             <h1 className="ProvidersHead">DATA Provider</h1>
+            <Grid container spacing={2}>
+                <Grid item xs={6} md={4}>
+                <Image src={require('../styles/img/Owlracle.png')} width={280} height={280} className="ImageProvider" alt ="Owlracle" />
+                </Grid>
+                <Grid item xs={6} md={4}>
+                <Image src={require('../styles/img/mempool.png')} width={367} height={85} className="ImageProvider" alt ="Owlracle" />
+                </Grid>
+            </Grid>
+        
         </Box>
 
 
